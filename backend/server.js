f99dfafd9788c9a453331398540b5bed3b8976cf
Snejana Stoyanova <snejka844@abv.bg>
2022-11-14 16:32:25 +0200

    Register, Login ,Home, auth

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,21 @@
 // const path = require('path')
-// const express = require('express')
-// require('colors')
-// require('dotenv').config()
-// const { errorHandler } = require('./middleware/errorMiddleware')
-// const connectDB = require('./config/db')
-// const PORT = process.env.PORT || 5000
+ const express = require('express')
+ require('colors')
+ require('dotenv').config()
+ const { errorHandler } = require('./middleware/errorMiddleware')
+ const connectDB = require('./config/db')
+ const PORT = process.env.PORT || 5000
 
-// // Connect to database
-// connectDB()
+ // Connect to database
+ connectDB()
 
-// const app = express()
+ const app = express()
 
-// app.use(express.json())
-// app.use(express.urlencoded({ extended: false }))
+ app.use(express.json())
+ app.use(express.urlencoded({ extended: false }))
 
-// // Routes
-// app.use('/api/users', require('./routes/userRoutes'))
+ // Routes
+ app.use('/api/users', require('./routes/userRoutes'))
 // app.use('/api/tickets', require('./routes/ticketRoutes'))
 
 // // Serve Frontend
@@ -28,11 +28,11 @@
 //     res.sendFile(path.join(__dirname, '../frontend/build/index.html'))
 //   })
 // } else {
-//   app.get('/', (_, res) => {
-//     res.status(200).json({ message: 'Welcome to the Support Desk API' })
-//   })
+   app.get('/', (_, res) => {
+     res.status(200).json({ message: 'Welcome to the Support Desk API' })
+   })
 // }
 
-// app.use(errorHandler)
+ app.use(errorHandler)
 
-// app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+ app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
